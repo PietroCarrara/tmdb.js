2fb3a68e0ede06966e328a270d66d40f75637b19
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2020-10-08 16:54:45 -0300

    Cache TV

--- tmdbcache.js
+++ tmdbcache.js
@@ -1,4 +1,4 @@
-import { Movie, TheMovieDB } from "./tmdb";
+import { Movie, TheMovieDB, TV } from "./tmdb";
 
 class TheMovieDBCacheProxy extends TheMovieDB {
 
@@ -6,6 +6,7 @@ class TheMovieDBCacheProxy extends TheMovieDB {
         super(key, lang);
 
         this.movie = new MovieCache(this);
+        this.tv = new TVCache(this);
     }
 }
 
@@ -66,6 +67,81 @@ class MovieCache extends Movie {
         }
 
         return popular;
+    }
+}
+
+class TVCache extends TV {
+    constructor(tmdb) {
+        super(tmdb);
+
+        this.detailsCache = {};
+        this.simpleDetailsCache = {};
+        this.seasonDetails = {};
+    }
+
+    async getDetails(id) {
+        if (this.detailsCache[id]) {
+            return this.detailsCache[id];
+        }
+
+        var details = await super.getDetails(id);
+        this.detailsCache[id] = details;
+        this.simpleDetailsCache[id] = details;
+
+        return details;
+    }
+
+    async getSimpleDetails(id) {
+        if (this.simpleDetailsCache[id]) {
+            return this.simpleDetailsCache[id];
+        }
+
+        return this.getDetails(id);
+    }
+
+    async getRecommended(id, page) {
+        var recommended = await super.getRecommended(id, page);
+
+        for (var show of recommended.response.results) {
+            this.simpleDetailsCache[show.id] = show;
+        }
+
+        return recommended;
+    }
+
+    async getSimilar(id, page) {
+        var similar = await super.getSimilar(id, page);
+
+        for (var show of similar.response.results) {
+            this.simpleDetailsCache[show.id] = show;
+        }
+
+        return similar;
+    }
+
+    async getPopular(page) {
+        var popular = await super.getPopular(page);
+
+        for (var show of popular.response.results) {
+            this.simpleDetailsCache[show.id] = show;
+        }
+
+        return popular;
+    }
+
+    async getSeasonDetails(id, seasonNumber) {
+        if (!this.seasonDetails[id]) {
+            this.seasonDetails[id] = {};
+        }
+
+        if (this.seasonDetails[id][seasonNumber]) {
+            return this.seasonDetails[id][seasonNumber];
+        }
+
+        var details = await super.getSeasonDetails(id, seasonNumber);
+        this.seasonDetails[id][seasonNumber] = details;
+
+        return details;
     }
 }
 
